cb7f05f20cf11f34084713234014760d03b12d03
Kuleb9lka <bogdan.lopatenko@gmail.com>
2024-02-05 20:46:42 +0300

    Initial commit

---
+++ src/test/java/SyberryBankApplication/Syberry/SyberryApplicationTests.java
@@ -0,0 +1,13 @@
+package SyberryBankApplication.Syberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyberryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
