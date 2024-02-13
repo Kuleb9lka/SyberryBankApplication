00e5ae0322c44471f1e3c93330e0e3e703baf5ad
Kuleb9lka <bogdan.lopatenko@gmail.com>
2024-02-13 21:33:01 +0300

    created user steps class

---
+++ src/main/java/SyberryBankApplication/Syberry/bot/model/UserSteps.java
@@ -0,0 +1,20 @@
+package SyberryBankApplication.Syberry.bot.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserSteps {
+
+    private long chatId;
+
+    private String bankName;
+
+    private String currencyName;
+
+
+
+}
