b9dead511b2accd4cea13350b4af4ee020a4ab7b
Kuleb9lka <bogdan.lopatenko@gmail.com>
2024-02-13 23:20:18 +0300

    created keyboards generator, created message sender

--- src/main/java/SyberryBankApplication/Syberry/api/constant/Constant.java
+++ src/main/java/SyberryBankApplication/Syberry/api/constant/Constant.java
@@ -13,5 +13,9 @@ public final class Constant {
     public static final String BOT_NAME = "Syberry_Bot_Java_Bot";
 
     public static final String BOT_TOKEN = "6534600404:AAGZSwhtDIHoeG-p4-umE2zw_unEyEQ3hMg";
+
+    public static final String START = "/start";
+
+
 }
 
--- src/main/java/SyberryBankApplication/Syberry/bot/service/SyberryBankBot.java
+++ src/main/java/SyberryBankApplication/Syberry/bot/service/SyberryBankBot.java
@@ -2,13 +2,18 @@ package SyberryBankApplication.Syberry.bot.service;
 
 
 import SyberryBankApplication.Syberry.api.constant.Constant;
+import SyberryBankApplication.Syberry.api.enums.SyberryBanks;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
@@ -31,8 +36,8 @@ public class SyberryBankBot extends TelegramLongPollingBot {
 
         List<BotCommand> botCommandList = new ArrayList<>();
 
-        botCommandList.add(new BotCommand("/start", "Start"));
-        try{
+        botCommandList.add(new BotCommand(Constant.START, "Start"));
+        try {
 
             this.execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
         } catch (TelegramApiException e) {
@@ -44,7 +49,81 @@ public class SyberryBankBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
+        Message message = update.getMessage();
+
+        if (message.hasText()) {
+
+            messageHandler(message);
+        }
 
 
+    }
+
+    private void messageHandler(Message message) {
+
+        if (message.hasText()) {
+
+            if (message.getText().equals(Constant.START)) {
+
+                initBanksKeyboard(message, SyberryBanks.getAllBanks());
+            }
+
+        }
+
+
+    }
+
+    private void initBanksKeyboard(Message message, List<String> buttons){
+
+        sendMessage(message, initKeyboard(buttons), "Выберите один из банков");
+    }
+
+    private List<KeyboardRow> initKeyboard(List<String> buttons) {
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+
+        KeyboardRow row = new KeyboardRow();
+
+        if (buttons.size() > 3) {
+
+            for (int i = 0; i < buttons.size(); i++) {
+
+                if (i % 2 == 0){
+
+                    keyboardRows.add(row);
+
+                    row = new KeyboardRow();
+                }
+
+                row.add(new KeyboardButton(buttons.get(i)));
+            }
+
+        } else {
+
+            for (String button : buttons) {
+
+                row.add(new KeyboardButton(button));
+            }
+        }
+
+        keyboardRows.add(row);
+
+        return keyboardRows;
+    }
+
+    private void sendMessage(Message message, List<KeyboardRow> rows, String text){
+
+        try{
+            execute(SendMessage.builder()
+                    .text(text)
+                    .replyMarkup(ReplyKeyboardMarkup.builder()
+                            .keyboard(rows)
+                            .resizeKeyboard(true)
+                            .build())
+                    .chatId(message.getChatId())
+                    .build());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
